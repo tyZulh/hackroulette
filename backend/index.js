60f9250ce52da0bafcf1e1c7075ac063909ea4df
Thomas Paoli <thomas.paoli.tp@gmail.com>
2020-04-29 19:32:53 +0200

    add socket.io and a socket connection handler

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
-const express = require('express')
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 const cors = require('cors')
 
-const app = express()
 const port = 4000
 
 app.use(cors())
 
-app.listen(port,err => {
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+})
+
+http.listen(port,err => {
   if(err) {
     throw new Error ('Ooopsie, sommething bad happened')
   }
